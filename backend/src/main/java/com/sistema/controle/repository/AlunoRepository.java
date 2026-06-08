d5fb99fcaf934c28d8a6fa7df1227855b9b66184
RICKZN <joaoriquelme12345@gmail.com>
2026-06-07 21:59:20 -0300

    Delete backend/src/main/java/com/sistema/controle directory

--- backend/src/main/java/com/sistema/controle/repository/AlunoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sistema.controle.repository;
-
-import com.sistema.controle.model.Aluno;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface AlunoRepository extends JpaRepository<Aluno, Long> {
-    Optional<Aluno> findByMatricula(String matricula);
-}
