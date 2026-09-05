330059e7178b24e4f5fdb2e02903bc63209802ce
RICKZN <joaoriquelme12345@gmail.com>
2026-09-05 11:55:33 -0300

    Update App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -199,3 +199,12 @@
                 </div>
                 <p>{{ item.quantidadeAtual }} {{ item.unidadeMedida }}</p>
                 <div style="display:flex; gap:6px; margin-top:1rem">
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  </div>
+</template>
